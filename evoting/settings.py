5411764d6267d808948598cecd02d9b33223ca7a
abhishek <abhishekpatidar2023@gmail.com>
2025-05-20 10:00:08 +0530

    commit 5

--- evoting/settings.py
+++ evoting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%6lp_p!%r$7t-2ql5hc5(r@)8u_fc+6@ugxcnz=h=b(fn#3$p9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [https://voting-6f3i.onrender.com]
+ALLOWED_HOSTS = []
 
 
 # Application definition
